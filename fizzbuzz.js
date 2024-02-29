ec473e4dad854f12c940c6f80f8d4967acc11955
Sara Duniphin <sara.duniphin@gmail.com>
2024-02-29 03:34:52 -0600

    Add files via upload

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,7 +1,7 @@
 var output = [];
-var count = 1;
+
 function fizzBuzz() {
-    while (count <= 100) {
+    for (var count = 1; count <= 100; count++) {
         if (count % 3 === 0 && count % 5 === 0) {
             output.push("FizzBuzz");
         } else if (count % 3 === 0) {
@@ -11,7 +11,7 @@ function fizzBuzz() {
         } else {
             output.push(count);
         }
-        count++;
+        
         console.log(output);
     }
 }
